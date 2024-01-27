59afca0ece66fd050590ff9127196a65f5d64db8
Ranjit Yadav <yadavranjit521@gmail.com>
2024-01-27 18:51:09 +0530

    init paytm clone app

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send("Hello duniya")
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
